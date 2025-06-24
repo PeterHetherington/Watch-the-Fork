c357f4a0a9e59c61532bd7b59f0826612ccbba2d
Sam Leader <sam.leader62@gmail.com>
2025-06-24 14:55:58 +0100

    movies get added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import pg from "pg"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pg from "pg";
 
 const app = express();
 
@@ -11,15 +11,34 @@ app.use(express.json());
 dotenv.config();
 
 const db = new pg.Pool({
-    connectionString: process.env.DB_CONN // .env variable
-})
+  connectionString: process.env.DB_CONN, // .env variable
+});
 
 // Routes
 // Root route
-app.get('/', (req, res) => {
-    res.json('on root route')
-})
+app.get("/", (req, res) => {
+  res.json("on root route");
+});
 
-app.listen(8080, ()  => {
-    console.log(`server running on port 8080`)
-})
+app.get("/movies", async (req, res) => {
+  const result = await db.query(`SELECT * FROM movies ORDER BY name ASC`);
+  res.json(result.rows);
+});
+
+app.get(`/movieReviews`, async (req, res) => {
+  const result = db.query(
+    `SELECT m.name, m.genre, m.description, m.rating, ROUND(AVG(mr.user_ratings), 1) AS avg_rating FROM movies m JOIN movie_reviews mr ON m.id = mr.movies_id GROUP BY m.name, m.genre, m.description, m.rating ORDER BY m.name ASC`
+  );
+  res.json((await result).rows);
+});
+
+app.get(`/randomMovie`, async (req, res) => {
+  const result = await db.query(
+    `SELECT m.name, m.genre, m.description, m.rating, ROUND(AVG(mr.user_ratings), 1) AS avg_rating FROM movies m JOIN movie_reviews mr ON m.id = mr.movies_id GROUP BY m.name, m.genre, m.description, m.rating ORDER BY RANDOM() LIMIT 1`
+  );
+  res.json((await result).rows);
+});
+
+app.listen(8080, () => {
+  console.log(`server running on port 8080`);
+});
