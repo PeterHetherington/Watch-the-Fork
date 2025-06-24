35f1b7d550fe90e1753cf61d6e3b6803b43ca271
Peter <peter.hetherington1123@gmail.com>
2025-06-24 13:59:33 +0100

    adds get routes

--- server/server.js
+++ server/server.js
@@ -20,6 +20,27 @@ app.get('/', (req, res) => {
     res.json('on root route')
 })
 
+// get game info
+app.get('/games', async (req, res) => {
+    const result = await db.query(`SELECT * FROM games ORDER BY name ASC`)
+    res.json(result.rows)
+})
+
+// get game + ratings
+app.get('/gameReviews', async (req, res) => {
+    const result = await db.query(`SELECT g.name, g.genre, g.description, g.maxplayers, ROUND(AVG(gr.rating), 1) AS avg_rating FROM games g JOIN gamereviews gr on g.id = gr.game_id GROUP BY g.name, g.genre, g.description, g.maxplayers ORDER BY g.name ASC`)
+    res.json(result.rows)
+}) 
+
+// get random game
+app.get('/randGame', async (req, res) => {
+    const result = await db.query(`SELECT g.name, g.genre, g.description, g.maxplayers, ROUND(AVG(gr.rating), 1) AS avg_rating FROM games g JOIN gamereviews gr on g.id = gr.game_id GROUP BY g.name, g.genre, g.description, g.maxplayers ORDER BY RANDOM() LIMIT 1`)
+    res.json(result.rows)
+})
+
+
+// get movies
+
 app.listen(8080, ()  => {
     console.log(`server running on port 8080`)
 })
